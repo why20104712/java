27bb356b6b8a3b8f7e0056835bf9e9fcc3381ff6
wanghaiyang <why20104712@gmail.com>
2017-07-13 21:45:57 +0800

    方法引用

--- src/main/java/com/why/MethodQuoteDemo.java
+++ src/main/java/com/why/MethodQuoteDemo.java
@@ -1,6 +1,7 @@
 package com.why;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -9,22 +10,22 @@ import java.util.List;
  * 2、引用对象的实例方法x::toString
  * 3、引用某个类型的任意对象的实例方法String::toString
  * 4、引用构造函数String::new
- *
- *
- 第一种方法引用的类型是构造器引用，语法是Class::new，或者更一般的形式：Class<T>::new。注意：这个构造器没有参数。
- final Car car = Car.create( Car::new );
- final List< Car > cars = Arrays.asList( car );
-
- 第二种方法引用的类型是静态方法引用，语法是Class::static_method。注意：这个方法接受一个Car类型的参数。
- cars.forEach( Car::collide );
-
- 第三种方法引用的类型是某个类的成员方法的引用，语法是Class::method，注意，这个方法没有定义入参：
- cars.forEach( Car::repair );
-
- 第四种方法引用的类型是某个实例对象的成员方法的引用，语法是instance::method。注意：这个方法接受一个Car类型的参数：
- final Car police = Car.create( Car::new );
- cars.forEach( police::follow );
-
+ * <p>
+ * <p>
+ * 第一种方法引用的类型是构造器引用，语法是Class::new，或者更一般的形式：Class<T>::new。注意：这个构造器没有参数。
+ * final Car car = Car.create( Car::new );
+ * final List< Car > cars = Arrays.asList( car );
+ * <p>
+ * 第二种方法引用的类型是静态方法引用，语法是Class::static_method。注意：这个方法接受一个Car类型的参数。
+ * cars.forEach( Car::collide );
+ * <p>
+ * 第三种方法引用的类型是某个类的成员方法的引用，语法是Class::method，注意，这个方法没有定义入参：
+ * cars.forEach( Car::repair );
+ * <p>
+ * 第四种方法引用的类型是某个实例对象的成员方法的引用，语法是instance::method。注意：这个方法接受一个Car类型的参数：
+ * final Car police = Car.create( Car::new );
+ * cars.forEach( police::follow );
+ * <p>
  * Created by why on 13/07/2017.
  */
 public class MethodQuoteDemo {
@@ -75,10 +76,26 @@ public class MethodQuoteDemo {
          */
         personInfoList.sort(PersonInfo::compare);
         print(personInfoList);
+
+
+//        第一种方法引用的类型是构造器引用，语法是Class::new，或者更一般的形式：Class<T>::new。注意：这个构造器没有参数。
+        final Car car = Car.create(Car::new);
+        final List<Car> cars = Arrays.asList(car);
+//        第二种方法引用的类型是静态方法引用，语法是Class::static_method。注意：这个方法接受一个Car类型的参数。
+        cars.forEach(Car::collide);
+//        第三种方法引用的类型是某个类的成员方法的引用，语法是Class::method，注意，这个方法没有定义入参：
+        cars.forEach(Car::repair);
+//        第四种方法引用的类型是某个实例对象的成员方法的引用，语法是instance::method。注意：这个方法接受一个Car类型的参数：
+        final Car police = Car.create(Car::new);
+        cars.forEach(police::follow);
+
+
     }
 
 
     public static void print(List<PersonInfo> personInfoList) {
         personInfoList.forEach(personInfo -> System.out.println(personInfo.getName()));
     }
+
+
 }
